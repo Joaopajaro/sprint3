58997ce54e8efd79c25a27400cc419e082ebc181
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-09-21 15:00:11 -0300

    Update IServicoArquivo.cs

--- Sprint3c/Sprint3/Sprint3.Core/Interfaces/IServicoArquivo.cs
+++ Sprint3c/Sprint3/Sprint3.Core/Interfaces/IServicoArquivo.cs
@@ -1,8 +1,8 @@
 namespace Sprint3.Core.Interfaces
 {
-    /// <summary>
+ 
     /// Contrato para importar ativos de arquivos.
-    /// </summary>
+
     public interface IServicoArquivo
     {
         void Importar(string caminho);
