97404781d96c5040fe27caaa65fb5f229d40ba71
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-09-21 15:01:00 -0300

    Update Cliente.cs

--- Sprint3c/Sprint3/Sprint3.Core/Models/Cliente.cs
+++ Sprint3c/Sprint3/Sprint3.Core/Models/Cliente.cs
@@ -2,24 +2,21 @@ using Sprint3.Core.Enums;
 
 namespace Sprint3.Core.Models
 {
-    /// <summary>
+
     /// Representa um cliente.
-    /// </summary>
+ 
     public class Cliente
-    {
-        /// <summary>
+    { 
         /// Identificador.
-        /// </summary>
+     
         public int Id { get; set; }
 
-        /// <summary>
         /// Nome do cliente.
-        /// </summary>
+ 
         public string Nome { get; set; } = string.Empty;
 
-        /// <summary>
         /// Perfil de investimento.
-        /// </summary>
+ 
         public Perfil Perfil { get; set; }
     }
 }
